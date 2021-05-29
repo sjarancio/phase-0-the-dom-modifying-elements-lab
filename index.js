7327811cd58679da84557d45a2aab025d8928959
Stephen Arancio <sjarancio@gmail.com>
2021-05-29 12:32:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.querySelector('#main')
+main.remove('main')
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader)
+
